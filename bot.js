856c6c75c4207fa5e1fea68cd3fd561a5866612c
Mr3bdo2030 <47391388+Mr3bdo2030@users.noreply.github.com>
2019-02-25 15:30:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -346,10 +346,10 @@ client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : EX Clan`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : دوخي ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`2play | Explotion`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
